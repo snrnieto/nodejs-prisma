bccdafac294cba94635ef65b12c1fb6cdccc0544
Sebastian Nieto <snrnieto1999@hotmail.com>
2024-08-29 14:43:00 -0500

    [ADD] findMany and findFirst

--- index.js
+++ index.js
@@ -3,13 +3,32 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const newUser = await prisma.user.create({
-    data: {
-      name: "Juan",
-      email: "juan@gmail.com",
+  // const newUser = await prisma.user.create({
+  //   data: {
+  //     name: "Juan",
+  //     email: "juan@gmail.com",
+  //   },
+  // });
+  // console.log(newUser);
+
+  const users = await prisma.user.findMany();
+  console.log(users);
+
+  const user = await prisma.user.findFirst({
+    where: {
+      // id: 2,
+      // OR: [
+      //   {
+      //     id: 1,
+      //   },
+      //   {
+      //     email: "maria@gmail.com",
+      //   },
+      // ],
     },
   });
-  console.log(newUser);
+
+  console.log(user);
 }
 
 main();
